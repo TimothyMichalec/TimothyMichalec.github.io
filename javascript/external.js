6e6152785bcabb598ae2264b769322bc18399c08
Michalec <tbm36@txstate.edu>
2019-09-23 20:33:17 -0500

    Saved username

--- javascript/external.js
+++ javascript/external.js
@@ -7,15 +7,16 @@
 //const y = "mapping";
 //const out = x + y;
 //document.write(out);
-// A prompt box is used to prompt users to input a value before entering a page.
-//user_name = window.prompt("Please enter your name", "Type your name here");
- //document.write(user_name);
+ //A prompt box is used to prompt users to input a value before entering a page.
+user_name = window.prompt("Please enter your name", "Type your name here");
+ document.write(user_name);
+ document.save(user_name);
 //x = 22;
 //const y = 33;
 //document.writeln(x + y);
 //document.writeln("<br>");
 //document.writeln(x += 33);
-document.writeln("<button onclick='condition()'>Conditional Test</button>")
+//document.writeln("<button onclick='condition()'>Conditional Test</button>")
 function condition ()
 {
   x = confirm("Are you sure you want to proceed?");
